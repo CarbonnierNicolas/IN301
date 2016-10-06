aec65bb33f915daef4c2dabf6d96e4c29cb672f9
CarbonnierNicolas <nicolaspro.carbonnier@gmail.com>
2016-10-06 13:06:02 +0200

    Ajout exo10

--- td2/Exo10.c
+++ td2/Exo10.c
@@ -3,11 +3,30 @@
 #include <time.h>
 
 
+int racine (int n)
+{
+	float epsilon = 0.001;
+	float g=0;
+	float d=n;
+	float m;
+	
+	while(d-g>epsilon)
+	{
+		m = (d+g)/2;
+		if(m*m>n)
+		{
+			d=m;
+		}
+		else
+		{
+			g=m;
+		}
+	}
+	return d;
+}
+
+
 int main()
 {
-
-
-
-
-
+	racine(5);
 }
