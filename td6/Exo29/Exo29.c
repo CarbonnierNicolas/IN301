ec2941e4ceab1d7476b6160d690dc231b1060036
CarbonnierNicolas <nicolaspro.carbonnier@gmail.com>
2016-12-01 14:10:35 +0100

    maj td6

--- td6/Exo29/Exo29.c
+++ td6/Exo29/Exo29.c
@@ -13,4 +13,20 @@ typedef struct{
 	pix **img;
 } image;
 
+int main(int argc, char *argv[]){
+	
+	FILE *F = fopen(argv[1], "r");
+	
+	char dump[256];
+	image test;
+	fscanf(F,"%s",&dump);
+	fscanf(F,"%d %d %d",&test.largeur, &test.longueur, &test.couleur_max);
+	test.img = malloc(largeur*sizeof(pix*));
+	for(int i = 0; i<largeur; i++){
+		
+		}
+}
+
+
+
 
